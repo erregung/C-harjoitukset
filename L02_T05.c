38eb2300eb5ed8dd4ee8c2669d50cd0a2f4ef037
erregung <kingelin@protonmail.com>
2021-03-06 23:31:58 +0200

    tee

--- L02_T05.c
+++ L02_T05.c
@@ -7,47 +7,57 @@ int main(void) {
     int valinta = 0;
     int pituus = 0;
     int flag = 1;
+	
+	printf("\n");
+	
+    while(flag) {
 
-    //while(flag) {
-
-    printf("Valikko\n1) Lisää uusi merkki\n2) Tyhjennä merkkijono\n3) Tulosta merkkijono\n0) Lopeta\nValintasi: \n");
+    printf("Valikko\n1) Lisää uusi merkki\n2) Tyhjennä merkkijono\n3) Tulosta merkkijono\n0) Lopeta\nValintasi: ");
 
     scanf("%d", &valinta);
-    printf("valinta: %d", valinta);
-    
+
     switch (valinta) {
         case 1:
-            printf("Anna jokin merkki: ");
+            if (pituus == 200) {
+                printf("Merkkijonoon ei mahdu enempää merkkejä.\n");
+                break;
+            }
+            printf("\nAnna jokin merkki: ");
             getchar();
             syote = getchar();
             merkkijono[pituus] = syote;
             pituus++;
+			printf("\n");
             break;
         case 2: 
             if (pituus == 0) {
                 printf("\nMerkkijono on tyhjä.\n");
                 break;
-            }   
+            }
+			printf("\nMerkkijono on tyhjennetty.\n\n");
             pituus = 0;
             break;
         case 3: 
             if (pituus == 0) {
-                printf("\nMerkkijono on tyhjä.\n");
+                printf("\nMerkkijono on tyhjä.\n\n");
                 break;
             }
             printf("\nMerkkijono: ");
             for (int i = 0; i <= pituus; i++) {
                 printf("%c", merkkijono[i]);
             }
-            printf("\n");
+			
+			printf("\n\n");
+			
             break;
         case 0: 
             printf("\nOhjelman lopetus.\n");
             flag = 0;
             break;  
         default: 
-            printf("\nTuntematon valinta.\n");
-    }//}
+            printf("\nTuntematon valinta.\n\n");
+    	}
+	}
 
 
 
